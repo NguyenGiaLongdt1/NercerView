df0cf4b0f0e84c94a93608e3ee67adc493a66673
NguyenGiaLongdt1 <151806349+NguyenGiaLongdt1@users.noreply.github.com>
2024-09-21 11:35:39 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using DevExpress.Xpf.Core;
+using NercerView.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NercerView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        private readonly HttpClient _httpClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient(); 
+        }
+
+        private async void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            string UserName = txtUsername.Text;
+            string PassWord = txtPassword.Password;
+
+           
+            var LoginRequest = new { UserName = UserName, PassWord = PassWord };
+            var JsonRequest = JsonSerializer.Serialize(LoginRequest);
+            var Content = new StringContent(JsonRequest, Encoding.UTF8, "application/json");
+            string ApiLogin = "http://localhost:5002/api/Auth/login";
+            try
+            {
+                if ( UserName != txtUsername.Text || PassWord != txtPassword.Password)
+                {
+                    MessageBox.Show("Sai thông tin tài khoải! Yêu cầu nhập lại thông tin");
+                    return;
+                }
+                //TrangChuNew TrangChuNew = new TrangChuNew();
+                var response = await _httpClient.PostAsync(ApiLogin, Content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = response.Content.ReadAsStringAsync();
+                    Application.Current.Properties["AuthToken"] = responseData;
+                    MessageBox.Show("Login Thành công");
+                    this.Close();
+                    //TrangChuNew.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void tbl_Register_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Register register = new Register();
+            register.ShowDialog();
+            this.Close();
+           
+        }
+    }
+}
---
+++ View/Register.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+
+
+namespace NercerView.View
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : ThemedWindow
+    {
+        private readonly HttpClient _httpClient;
+        public Register()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+        }
+
+        private async void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            var RegisterData = new
+            {
+                Name = txtName.Text,
+                Username = txtUserName.Text,
+                Email = txtEmail.Text,
+                Phone = txtSDT.Text,
+                Password = txtPassword.Password,
+                ConfirmPassword = txtConfirmPassword.Password,
+            };
+            if (RegisterData.Password.Length < 5)
+            {
+                MessageBox.Show("PassWord phải lớn hơn 5 ký tự");
+                return;
+            }
+            if ((int)RegisterData.Email.Count(e => e == '@') != 1)
+            {
+                MessageBox.Show("Email không hợp lệ, phải chứa một ký tự @");
+                return;
+            }
+            if (RegisterData.Password != RegisterData.ConfirmPassword)
+            {
+                MessageBox.Show("Mật khẩu không trùng khớp! Yêu cầu ConfirmPassword trùng khớp");
+                return;
+            }
+
+            string ApiUri = "http://localhost:5002/api/Auth/register";
+
+            _httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var json = JsonSerializer.Serialize(RegisterData);
+            Console.WriteLine(json);
+            var Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage httpResponseMessage = await _httpClient.PostAsync(ApiUri, Content);
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                MessageBox.Show(" Đăng ký thành công");
+                this.Close();
+                MainWindow main = new MainWindow();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+    }
+}
